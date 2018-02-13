69e5aa18f7b2f3c1bcc791ab6079f1f7cb5a7b35
leesegal <segall2@uw.edu>
2018-02-13 09:44:45 -0800

    testing

--- js-dom/activity-1/js/index.js
+++ js-dom/activity-1/js/index.js
@@ -32,18 +32,29 @@ let state = {
 // Also, set the pointerEvents to 'none'. Then, append this `rect` to the svg
 
 
+
 // Create a button with class "btn center-align" and append it to the element with class container.
 // You want to have it *inserted before* the div with class `svg-wrapper`
+let button = document.createElement('button');
+button.className = "btn center-align";
+let svgWrapper = document.querySelector('.svg-wrapper');
+document.querySelector('.container').insertBefore(button, svgWrapper);
+button.textContent = 'clear';
 
 
 // Function to render a circle in a parent element
 function renderCircle(coord, parent) {
     // Create the circle
-
-
+    let circle = document.createElementNS(namespace, 'circle');
+    circle.setAttribute('cx', coord.x);
+    circle.setAttribute('cy', coord.y);
+    circle.setAttribute('r', 10);
     // Define styles
-
+    circle.style.opacity = 0.3;
+    circle.style.pointerEvents = 'none';
+    circle.style.fill = 'rgb(0, ${coor.x"}, ${coor.y})';
     // Append
+    parent.appendChild(circle);
 
 }
 
@@ -60,6 +71,10 @@ function clearCircles(parent) {
 // Assign event listener - on click:
 // - push a new data element into the state
 // - call the `update` function
+svg.addEventListner('click', function(event) {
+    console.log(event); //shares all the information about the object event
+    state.circles.push({x:event})
+} );
 
 
 // Update function: call `clearCircles`, `drawCircles`, and `updateText`
@@ -74,7 +89,13 @@ function update() {
 // Avg. color (in rgb) is the avg. x position (green) and avg. y position (blue)
 // Set the text to be that color
 function updateText(data) {
+    let p = document.querySelector('p');
     // Compute average in x direction (green)
+    let green = data.reduce(function(a, b) {
+        console.log(a,b);
+        return a + b.x;
+    }, 0); 
+
 
 
     // Compute average in x y direction (blue)
